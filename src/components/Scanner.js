787350a6e2c30917330ed037aff7817637e3880c
bashar021 <alambashar021@gmail.com>
2023-07-23 12:14:49 +0530

    checking

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,13 +1,14 @@
 
-import React, { useState, useRef,useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 // import QrReader from 'react-qr-reader';
 import QrReader from 'react-qr-scanner'
 
 export default function Appp() {
     const [selectedCamera, setSelectedCamera] = useState(null);
     const [cameras, setCameras] = useState([]);
-    const [qrcodevalue,setqrcodevalue] = useState("");
+    const [qrcodevalue, setqrcodevalue] = useState("");
     const qrReaderRef = useRef(null);
+    const [scan, setScan] = useState(false)
 
     const handleScan = (data) => {
         if (data) {
@@ -20,39 +21,38 @@ export default function Appp() {
         console.error('QR scan error:', error);
     };
     const handleStartScan = (selectedDeviceId) => {
-       console.log(selectedCamera)
         const v = document.querySelector('video');
         if (selectedDeviceId) {
             const constraints = {
-            video: {
-                facingMode:"environment",
-                deviceId: selectedDeviceId 
-            },
+                video: {
+                    facingMode: "environment",
+                    deviceId: selectedDeviceId
+                },
             };
             // { video: {  deviceId: { exact: d} } }
-                navigator.mediaDevices.getUserMedia(constraints)
+            navigator.mediaDevices.getUserMedia(constraints)
                 .then((stream) => {
                     v.srcObject = stream;
-                    v.openImageDialog(); 
+                    // v.openImageDialog();
                 })
                 .catch((error) => {
                     console.error('Error accessing the camera:', error);
                 });
         }
-        
+
     }
     const handleCameraChange = (event) => {
         const selectedDeviceId = event.target.value;
         setSelectedCamera(selectedDeviceId);
         handleStartScan(selectedDeviceId)
     };
-   
+
 
     const enumerateCameras = async () => {
         try {
             const devices = await navigator.mediaDevices.enumerateDevices();
             const cameras = devices.filter((device) => device.kind === 'videoinput');
-             setCameras(cameras)
+            setCameras(cameras)
             return cameras;
         } catch (error) {
             console.error('Error enumerating cameras:', error);
@@ -66,42 +66,41 @@ export default function Appp() {
         const getCameras = async () => {
             const cameras = await enumerateCameras();
             // console.log(cameras)
-            if (cameras.length > 0) {
+            // if (cameras.length > 0) {
                 // Set the first available camera as the default selectedCamera
                 setSelectedCamera(cameras[0].deviceId);
-            }
+            // }
         };
+        console.log(selectedCamera)
         getCameras();
     }, [0]);
 
     return (
-        <>  
+        <>
             <div>
                 <select value={selectedCamera} onChange={handleCameraChange}>
                     {cameras.map((camera) => (
                         <option key={camera.deviceId} value={camera.deviceId}>
                             {camera.label}
                         </option>
-                       
+
                     ))}
-                     <option value="bashar">bashar</option>
+                    <option value="bashar">bashar</option>
                 </select>
 
             </div>
-            <div>
-               
-                <QrReader
-                   
-                    delay={300}
-                    onError={handleError}
-                    onScan={handleScan}
-                    // constraints = {cameras > 1 ?{facingMode :'environment'} :{facingMode :{exact:'user'}}}
-                    constraints = {{facingMode :{ exact: 'environment', deviceId: selectedCamera }}}
+            {
+                scan == false ? <div>
+                    <QrReader delay={300} onError={handleError} onScan={handleScan} style={{ width: '50%' }}
+                        // constraints = {cameras > 1 ?{facingMode :'environment'} :{facingMode :{exact:'user'}}}
+                        // constraints={{ facingMode: { exact: 'environment'} }}
                     // facingMode={selectedCamera ? { exact: 'environment', deviceId: selectedCamera } : 'environment'}
-                    style={{ width: '50%'}}
-                />
-                {/* <p>{qrcodevalue}</p> */}
-            </div>
+                    />
+                    {/* <p>{qrcodevalue}</p> */}
+                </div>:<p></p>
+            }
+
+            <button onClick={function () { setScan(false); handleStartScan(selectedCamera); console.log(selectedCamera); }}>Scan Ticket</button>
         </>
     )
 }
